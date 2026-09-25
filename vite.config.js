74718e417f920a7c1a5c6bb1a5e49f817569e2f2
Sansalvasur <sssinnovacion@gmail.com>
2026-09-25 14:09:57 -0600

    Change mapa input file from index.html to map.html

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
         rollupOptions: {
             input: {
                 main: 'index.html',
-                mapa: 'mapa/index.html'
+                mapa: 'mapa/map.html'
             }
         }
     },
